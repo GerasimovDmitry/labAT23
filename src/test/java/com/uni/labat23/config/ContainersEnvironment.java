c2acd0ff022ee1c3cd4aad94db2aad4b9a117d31
GerasimovDmitry <gerasimovdm17@gmail.com>
2022-12-22 21:33:42 +0400

    Initial commit

---
+++ src/test/java/com/uni/labat23/config/ContainersEnvironment.java
@@ -0,0 +1,16 @@
+/*
+package config;
+
+import com.example.demo.PostgreTestContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.*;
+
+
+@Testcontainers
+public class ContainersEnvironment {
+
+   @Container
+   public static PostgreSQLContainer postgreSQLContainer = PostgreTestContainer.getInstance();
+}
+
+*/
---
+++ src/test/java/com/uni/labat23/runner/CucumberIntegrationTest.java
@@ -0,0 +1,20 @@
+/*
+package runner;
+
+import com.uni.labat23.LabAt23Application;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        glue = { "com.example.labs2.definitions"},
+        features = "src/test/resources/features")
+@CucumberContextConfiguration
+@SpringBootTest(classes = LabAt23Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class CucumberIntegrationTest {
+}
+*/
